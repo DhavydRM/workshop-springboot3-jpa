e5f2bdb0364f161670253e276b8cae793b551391
DhavydRM <dhavydromano@gmail.com>
2025-07-22 23:20:59 -0300

    Category

--- src/main/java/com/projectimperium/course/config/TestConfig.java
+++ src/main/java/com/projectimperium/course/config/TestConfig.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.Profile;
 import com.projectimperium.course.entities.Order;
 import com.projectimperium.course.entities.User;
 import com.projectimperium.course.entities.enums.OrderStatus;
+import com.projectimperium.course.repositories.CategoryRepository;
 import com.projectimperium.course.repositories.OrderRepository;
 import com.projectimperium.course.repositories.UserRepository;
+import com.projectimperium.course.entities.Category;
 
 @Configuration
 @Profile("test")
@@ -24,8 +26,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999999", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "9919929290", "123456");
